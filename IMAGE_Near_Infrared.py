1b5d3026cc280119f5abde354ee034c0e84c8a70
Changwan Sun <tjsckddhks@gmail.com>
2022-04-12 01:04:31 +0900

    Program to detect changes in the K3A images caputered in Jeju before and after 2021 Jeju earthquake.

---
+++ IMAGE_Near_Infrared.py
@@ -0,0 +1,118 @@
+
+import glob
+from os.path import isdir
+import os
+#import pygmt
+import sys
+from osgeo import ogr, osr, gdal, gdalconst, gdal_array
+import numpy as np
+import matplotlib.pyplot as plt
+
+#data_dir='/home/changwan/IMAGE/'
+#fp=os.path.join("LC08_L2SP_115035_20210323_20210402_02_T1_SR_B2.TIF")
+#print (fp)
+#os.chdir(data_dir)
+
+
+#Define a function to normalize each band array by the 
+#min and max values.
+
+#b4_file = glob.glob('**_N.tif') #near infrared band
+#b3_file = glob.glob('**B3.TIF') #green band
+#b4_file = glob.glob('**B4.TIF') #red band
+
+#print(b2_file)
+#print(b3_file)
+#print(b4_file)
+#b2_file.type
+#fp=os.path.join(data_dir, b4_file)
+
+fp_r="/home/changwan/IMAGE/K3A_20200323060952_27569_00037135_L1R_R.tif"
+fp_g="/home/changwan/IMAGE/K3A_20200323060952_27569_00037135_L1R_G.tif"
+fp_b="/home/changwan/IMAGE/K3A_20200323060952_27569_00037135_L1R_B.tif"
+#fp_n="/home/changwan/IMAGE/K3A_20200323060952_27569_00037135_L1R_N.tif"
+
+
+#print(fp)
+
+def norm(band):
+    band_min, band_max = band.min(), band.max()
+    return ((band - band_min)/(band_max - band_min))
+
+#Open each band using gdal
+#list to string with "".join function
+#b2_file="".join(b2_file)
+#b3_file="".join(b2_file)
+#b4_file="".join(b2_file)
+
+#b2 = gdal.Open(b2_file)
+#b3 = gdal.Open(b3_file)
+b_r = gdal.Open(fp_r)
+b_g = gdal.Open(fp_g)
+b_b = gdal.Open(fp_b)
+#b_n = gdal.Open(fp_n)
+
+#Call the norm function on each band as array converted to float          
+b_r = norm(b_r.ReadAsArray().astype(float))
+b_g = norm(b_g.ReadAsArray().astype(float))
+b_b = norm(b_b.ReadAsArray().astype(float))
+#b_n = norm(b_n.ReadAsArray().astype(float))
+
+
+np.seterr(divide='ignore', invalid='ignore')
+
+#Band Ratio
+#b_ratio = np.divide(b_r, b_n)
+
+print(b_r.shape[1])
+print(len(b_r))
+
+xx = len(b_r)
+yy = b_r.shape[1]
+
+x_r = range(0, xx-1, 1)
+y_r = range(0, yy-1, 1)
+
+b_ratio = np.zeros([xx, yy])
+
+for x in x_r:
+ for y in y_r:    
+  b_ratio[x,y] = b_r[x,y] / b_b[x,y]
+
+#for x in range(0,100,1):
+# for y in range(0,100,1):    
+#  print(b_ratio[x,y])
+
+
+#Create RGB
+RGB = np.dstack((b_b,b_g,b_r))
+
+
+
+# Visualize RGB
+#plt.imshow(b_n[1000:3000,1000:3000], cmap='RdBu_r')
+plt.subplot(2, 1, 1)
+plt.imshow(b_ratio[1000:3000,1000:3000], cmap='RdBu_r')
+#plt.imshow(b_ratio, cmap='RdBu_r')
+plt.colorbar()
+
+plt.subplot(2, 1, 2)
+plt.imshow(RGB[1000:3000,1000:3000])
+
+plt.tight_layout()
+plt.show()    
+    
+"""
+    # Export RGB as TIFF file
+    # Important: Here is where you can set the custom stretch
+    # I use min as 2nd percentile and max as 98th percentile
+    #sm.toimage(rgb,cmin=np.percentilnumpy.ndarray' object has no attribute 'ReadAsArraye(rgb,2),cmax=np.percentile(rgb,98)).save(b2_file[i].split('_01_')[0]+'_RGB.tif')
+RGB.min()    
+
+#Get nodata value from the GDAL band object
+nodata = RGB.GetNoDataValue()
+
+#Create a masked array for making calculations without nodata values
+RGB = np.ma.masked_equal(RGB,nodata)
+type(RGB)
+"""
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Photoclinometry
+
+Consider the solar illumination angle in the different dates.
+
+You can find 'sunangle' in the xml files of K3A.
+
